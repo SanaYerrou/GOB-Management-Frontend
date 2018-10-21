5cb36ed2290f0a909208b5b347601c6baf5ff248
vegter <amsjurien@gmail.com>
2018-10-21 15:43:14 +0200

    Enable local hot-reload

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === "production" ? "/GOB-Management-Frontend/" : "/"
+  baseUrl: process.env.NODE_ENV === "production" ? "/GOB-Management-Frontend/" : "/",
+  devServer: {
+    hot: true,
+    disableHostCheck: true,
+    host: "localhost",
+    headers: {
+      'Access-Control-Allow-Origin': 'http://localhost:8080/',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Max-Age': '3600',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-id, Content-Length, X-Requested-With',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    }
+  }
 };
