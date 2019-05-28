81dc2f2a96e7b1c3a3b8a7e6c8d35c5e60cc3c4d
vegter <amsjurien@gmail.com>
2019-05-28 13:28:02 +0200

    Pass keycloak token
    Include test code to check results

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <b-card tag="article" style="max-width: 20rem;" class="mt-2 mb-2 mx-auto">
-      <img alt="GOB logo" src="../assets/logo.png" />
+      <img alt="GOB logo" src="../assets/logo.png" @click="testSecure();" />
       <p class="mt-2 card-text">
         <b>G</b>enerieke <b>O</b>ntsluiting <b>B</b>asisgegevens
       </p>
@@ -16,8 +16,15 @@
 </template>
 
 <script>
+import { getSecure } from "../services/gob";
+
 export default {
   name: "home",
+  methods: {
+    testSecure() {
+      getSecure();
+    }
+  },
   mounted() {
     if (this.$route.query.p) {
       this.$router.push(this.$route.query.p);
